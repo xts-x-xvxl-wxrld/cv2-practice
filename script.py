b7b58e32c352ebbdc5d03bf0e60f6436a63d895a
Ravil <pink.ravil02@gmail.com>
2022-10-20 12:59:20 +0200

    Add files via upload
    This script executes motion detection and returns the timestamps of objects appearing and disappearing from frame .

--- script.py
+++ script.py
@@ -1,19 +1,51 @@
 import cv2
+from datetime import datetime
 
+first_frame = None
+video = cv2.VideoCapture(0)
+status_list = [None, None]
+times = []
+while True:
+    check, frame = video.read()
+    status = 0
 
-front_cascade = cv2.CascadeClassifier('head_front_cascade.xml')
-path = 'Before.png'
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-image = cv2.imread(path, 1)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blur = cv2.GaussianBlur(gray, [5,5], cv2.BORDER_DEFAULT)
+    if first_frame is None:
+        first_frame = gray
+        continue
 
-faces = front_cascade.detectMultiScale(gray,
-                                       scaleFactor=1.1,
-                                       minNeighbors=3)
+    delta = cv2.absdiff(first_frame, gray)
+    thresh_frame = cv2.threshold(delta, 50, 255, cv2.THRESH_BINARY)[1]
+    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
 
-for x, y, w, h in faces:
-    #cv2.rectangle(image, (x, y), (x+w, y+h), (0, 0, 0), 2)
-    extract = image[y:y+h, x:x+w]
-    cv2.imwrite('scalp.jpg', extract)
+    (cnts, _) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    for contour in cnts:
+        if cv2.contourArea(contour) < 10000:
+            continue
+        status = 1
+
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    status_list.append(status)
+
+    if status_list[-1] == 1 and status_list[-2] == 0:
+        times.append(datetime.now())
+    if status_list[-1] == 0 and status_list[-2] == 1:
+        times.append(datetime.now())
+
+    cv2.imshow('capture', gray)
+    cv2.imshow('delta', delta)
+    cv2.imshow('thresh', thresh_frame)
+    cv2.imshow('color frame ', frame)
+
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+print(times)
+video.release()
+cv2.destroyAllWindows()
